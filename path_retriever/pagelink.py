35bd3138702adbe547ef9312480794933adf6f25
Pham Duc Hoang <hoangggp@gmail.com>
2025-09-05 23:05:57 +0700

    test

--- path_retriever/pagelink.py
+++ path_retriever/pagelink.py
@@ -103,10 +103,10 @@ set_seed(0)
 processed_g = load_dataset(args.dataset_dir, args.dataset_name, args.split, args.valid_ratio, args.test_ratio, args.stage)[1]
 mp_g, train_pos_g, train_neg_g, val_pos_g, val_neg_g, test_pos_g, test_neg_g = [g.to(device) for g in processed_g]
 
-# encoder = HeteroRGCN(mp_g, args.emb_dim, args.hidden_dim, args.out_dim)
-# model = HeteroLinkPredictionModel(encoder, args.src_ntype, args.tgt_ntype, args.link_pred_op, **pred_kwargs)
-encoder = LightGCN(mp_g, args.emb_dim, 2)
+encoder = HeteroRGCN(mp_g, args.emb_dim, args.hidden_dim, args.out_dim)
 model = HeteroLinkPredictionModel(encoder, args.src_ntype, args.tgt_ntype, args.link_pred_op, **pred_kwargs)
+# encoder = LightGCN(mp_g, args.emb_dim, 2)
+# model = HeteroLinkPredictionModel(encoder, args.src_ntype, args.tgt_ntype, args.link_pred_op, **pred_kwargs)
 # state = torch.load(f'{args.saved_model_dir}/{args.saved_model_name}_{args.split}_lightgcn.pth', map_location='cpu')
 state = torch.load(f'{args.saved_model_dir}/{args.dataset_name}_model_{args.split}.pth', map_location='cpu')
 
