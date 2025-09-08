f440bab0c5b6c4239f8774a7a898a2b44a34d74b
Pham Duc Hoang <hoangggp@gmail.com>
2025-09-08 18:19:31 +0700

    test

--- path_retriever/data_processing.py
+++ path_retriever/data_processing.py
@@ -135,7 +135,10 @@ def load_dataset(dataset_dir, dataset_name, split, val_ratio, test_ratio, stage)
     graph_saving_path = f'{dataset_dir}/{dataset_name}/{split}_graph.bin'
     print(f'graph loading from: {graph_saving_path}')
     graph_list, _ = dgl.load_graphs(graph_saving_path)
+
+    print('-'*30)
     print(f'graph loaded: {graph_list}')
+    print('-'*30)
     # pred_pair_to_edge_labels = torch.load(f'{graph_saving_path}_pred_pair_to_edge_labels')
     # pred_pair_to_path_labels = torch.load(f'{graph_saving_path}_pred_pair_to_path_labels')
     g = graph_list[0]
