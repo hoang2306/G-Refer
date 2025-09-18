71ce156bc39fb740c65b17d4b6c1aeb2d338a266
Pham Duc Hoang <hoangggp@gmail.com>
2025-09-18 17:22:24 +0700

    test

--- path_retriever/pagelink.py
+++ path_retriever/pagelink.py
@@ -105,10 +105,16 @@ print_args(args)
 set_seed(0)
 
 processed_g = load_dataset(args.dataset_dir, args.dataset_name, args.split, args.valid_ratio, args.test_ratio, args.stage)[1]
+if args.debug:
+    print(f'processed_g: {processed_g}')
+    slash()
+
 mp_g, train_pos_g, train_neg_g, val_pos_g, val_neg_g, test_pos_g, test_neg_g = [g.to(device) for g in processed_g]
 
 if args.debug:
+    # if error, mp_g have no edges! 
     print(f'mp_g: {mp_g}')
+    slash()
 
 # print(f'processed_g: {processed_g}')
 # print(f'graph mg_g: {mp_g}')
