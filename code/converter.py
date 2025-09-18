d0997771043e60bb7539169e34f8f7cc6c06529e
Pham Duc Hoang <hoangggp@gmail.com>
2025-09-18 14:07:46 +0700

    test

--- code/converter.py
+++ code/converter.py
@@ -106,6 +106,7 @@ def main():
     # item_combined_texts = [f"Title: {title}\nSummary: {text}" for title, text in zip(item_titles, item_texts)]
     
     all_texts = ordered_user_texts + ordered_item_texts
+    print(f'len of all_texts: {len(all_texts)}') if args.debug else None
 
     text_model = TextModel(args.text_encoder)
 
