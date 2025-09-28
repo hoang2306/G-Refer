920b5a8fc728335abb5b4d3737cc32b5d974085e
Pham Duc Hoang <hoangggp@gmail.com>
2025-09-28 10:34:56 +0700

    test

--- code/translation.py
+++ code/translation.py
@@ -247,7 +247,7 @@ def slash():
 def main():
     args = parse_args()
 
-    mapper = DataMapper(f"data/{args.dataset}/data_{args.split}.pt", debug=args.debug)
+    mapper = DataMapper(f"data/{args.dataset}/data_{args.split}.pt", debug_mode=args.debug)
 
     # load dense retrieval results
     with open(
